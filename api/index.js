d1a40450b4c60fbab556c27662404fad6c819387
anilsebastin <anilsebastin@gmail.com>
2023-10-09 19:54:48 +0530

    create listing API route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
+
 dotenv.config();
 
 mongoose
@@ -26,6 +28,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 //Middleware
 app.use((err, req, res, next) => {
