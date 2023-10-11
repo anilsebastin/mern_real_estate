c3d39efd922ff868e6789cdba0cdc61f60bb8f6f
anilsebastin <anilsebastin@gmail.com>
2023-10-11 15:18:56 +0530

    Add contact landlord functionality to listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -14,6 +14,7 @@ import {
   FaShare,
 } from "react-icons/fa";
 import { useSelector } from "react-redux";
+import Contact from "./Contact";
 
 function Listing() {
   SwiperCore.use([Navigation]);
@@ -135,6 +136,15 @@ function Listing() {
                 {listing.furnished ? "Furnished" : "Unfurnished"}
               </li>
             </ul>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button
+                onClick={() => setContact(true)}
+                className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3"
+              >
+                Contact landlord
+              </button>
+            )}
+            {contact && <Contact listing={listing} />}
           </div>
         </div>
       )}
